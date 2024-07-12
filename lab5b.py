3d4037cd46af1db52709466b844f0aea771f0dc6
Israel <iaoluwajuwon@myseneca.ca>
2024-07-11 23:58:04 -0400

    Lab5 completed.

---
+++ lab5b.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# Author ID: iaoluwajuwon
+
+def read_file_string(file_name):
+    # Takes file_name as string for a file name, returns its entire contents as a string
+    f = open(file_name, 'r')
+    read_file = f.read()
+    f.close()
+    return read_file
+
+def read_file_list(file_name):
+    # Takes a file_name as string for a file name, 
+    # return its entire contents as a list of lines without new-line characters
+    f = open(file_name, 'r')
+    read_file = f.readlines()
+    f.close()
+    read_file = [line.strip() for line in read_file]
+    return read_file
+
+
+def append_file_string(file_name, string_of_lines):
+    # Takes two strings, appends the string to the end of the file
+    f = open(file_name, 'a')
+    f.write(string_of_lines)
+    f.close()
+
+def write_file_list(file_name, list_of_lines):
+    # Takes a string and list, writes all items from list to file where each item is one line
+    f = open(file_name, 'w')
+    for line in list_of_lines:
+            f.write(line + '\n')
+    f.close()
+
+def copy_file_add_line_numbers(file_name_read, file_name_write):
+    # Takes two strings, reads data from first file, writes data to new file, adds line number to new file
+    read_file = open(file_name_read, 'r')
+    file_name_read = read_file.readlines()
+    read_file.close()
+
+    write_file = open(file_name_write, 'w')
+    for i, line in enumerate(file_name_read, 1):
+        write_file.write(f"{i}:{line}")
+    write_file.close()
+
+if __name__ == '__main__':
+    file1 = 'seneca1.txt'
+    file2 = 'seneca2.txt'
+    file3 = 'seneca3.txt'
+    string1 = 'First Line\nSecond Line\nThird Line\n'
+    list1 = ['Line 1', 'Line 2', 'Line 3']
+    append_file_string(file1, string1)
+    print(read_file_string(file1))
+    write_file_list(file2, list1)
+    print(read_file_string(file2))
+    copy_file_add_line_numbers(file2, file3)
+    print(read_file_string(file3))
